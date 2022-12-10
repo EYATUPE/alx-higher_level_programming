47f0a51a383b9431a5a1a887c7761990ce67d6b0
EYATUPE <eyatuphilipesamu@gmail.com>
2022-12-10 00:31:54 -0800

    Print postive of negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
+
 if number > 0:
-	print(f'{number:d} is postive')
+   print(f'{number:d} is postive')
 elif number == 0:
-	print(f'{number:d} is zero')
+   print(f'{number:d} is zero')
 elif number < 0:
-	print(f'{number:d} is negative')
+   print(f'{number:d} is negative')
