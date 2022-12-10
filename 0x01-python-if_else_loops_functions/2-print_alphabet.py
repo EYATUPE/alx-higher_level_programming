4c3069e9962b1bb7be2b60d2d0422d2e34be69e1
EYATUPE <eyatuphilipesamu@gmail.com>
2022-12-10 02:07:31 -0800

    prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-# input a number
-s=int(input('Enter an ASCII value: '))
-# getting its ascii character
-n=chr(s)
-#Print the Results
-print('ASCII of value {} is {}.'.format(s,n))
+for alpha_letters in range(ord('a'), ord('z')+1):
+	print("{:c}".format(alpha_letters), end="")
---
+++ 0x01-python-if_else_loops_functions/demo-print_alphatet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# input a number
+s=int(input('Enter an ASCII value: '))
+# getting its ascii character
+n=chr(s)
+#Print the Results
+print('ASCII of value {} is {}.'.format(s,n))
