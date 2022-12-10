f11505ec8b816563212315959bcc7cc274907b74
EYATUPE <eyatuphilipesamu@gmail.com>
2022-12-10 07:29:46 -0800

    prints all numbers from 0 to 98 in decimal and in hexadecimal

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for s in range (0,9):
-    for e in range (s + 1, 10):
+for s in range(0, 9):
+    for e in range(s + 1, 10):
         if s == 8:
             print("{}{}".format(s, e))
-	else:
+else:
 	    print("{}{}".format(s, e), end=", ")
