d41a70b44f1c631d7404a7bf19f1db8c010e88f7
EYATUPE <eyatuphilipesamu@gmail.com>
2022-12-10 04:56:20 -0800

    prints the ASCII alphabet, eliminating some

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z')+1):
-    if chr(alpha_letters) != 'e' and chr(alpha_letters) != 'q':
-       print("{:c}".format(alpha_letters), end="")
+for a in range(ord('a'), ord('z')+1):
+    if chr(a) != 'e' and chr(a) != 'q':
+       print("{:c}".format(a), end="")
