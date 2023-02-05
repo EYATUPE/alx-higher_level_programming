c25719cdd75778388696ce8b4f265b8af2e225e5
EYATUPE <eyatuphilipesamu@gmail.com>
2023-02-05 13:47:26 -0800

    Magic String

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("Best, " * (magic_string.n - 1) + "School")
+def magic_string(values=[]):
+    values.append('Best')
+    return ', '.join(values)
