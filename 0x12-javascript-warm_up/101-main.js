6ed193de46786ce7541d2fad42652b76d84a830a
EYATUPE <eyatuphilipesamu@gmail.com>
2023-03-15 06:38:38 -0700

    a function that executes x times a function

--- 0x12-javascript-warm_up/101-main.js
+++ 0x12-javascript-warm_up/101-main.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-module.exports = {
-  /**
-   * callMeMoby - Invokes a function x times.
-   * @param {Number} x - The number of times to invoke the function.
-   * @param {Number} theFunction - The function to be invoked.
-   */
-  callMeMoby: function (x, theFunction) {
-    for (let i = 0; i < x; i++) {
-      theFunction();
-    }
-  }
-};
-
+const callMeMoby = require('./101-call_me_moby').callMeMoby;
+callMeMoby(3, function () {
+  console.log('C is fun');
+});
